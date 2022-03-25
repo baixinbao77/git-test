72a284f967d0235f204d22612b6d07d6accad5d0
bxb <bxb15554198772@aliyun.com>
2022-03-25 11:11:52 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author BXBstart
+ * @create 2022-03-25 11:07
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
